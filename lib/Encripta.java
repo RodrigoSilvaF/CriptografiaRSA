944f8c43e41c03b84c3878f1498d0d72967a3789
Rodrigo Silva <Rodrigo-7491@hotmail.com>
2022-01-10 16:07:58 -0300

    Encriptar

--- lib/Encripta.java
+++ lib/Encripta.java
@@ -2,26 +2,6 @@ import java.math.*;
 
 
 public class Encripta {
-    static int mdc(int a, int b){
-        while(b != 0){
-            int r = a % b;
-            a = b;
-            b = r;
-        }
-        return a;
-    }
-
-    static int mmc(int a, int b){
-        return (a * (b / mdc(a, b)));
-    }
-
-    static int inversomult(int a, int m){
-        for (int x = 1; x < m; x++)
-            if (((a%m) * (x%m)) % m == 1){
-                return x;
-            }
-        return 0;
-    }
 
     //A = Mensagem, E = Coprime entre 1 < e < MMC(P-1,Q-1), N = PRIMO 1 * PRIMO 2, KEY = KEY ;)
     static BigInteger encriptar(BigInteger a){
